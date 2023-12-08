4e3200f41f8bde8dc096aa78a2bb55cbd5f57dc4
MYJCONAN-work-DELL <273987074@qq.com>
2023-12-08 18:09:41 +0800

    no message

--- jenkinsfile/build_parameters.groovy
+++ jenkinsfile/build_parameters.groovy
@@ -1,5 +1,5 @@
 //project_type
-def list=" bash  /var/jenkins_home/jobs/qa-k8s-env-for-production-project/mod_git_base/jenkinsfile/qa-k8s-env-for-production-project.sh db_query_project_type_all".execute().in.text;
+def list=" bash /var/jenkins_home/jobs/qa-k8s-env-for-production-project/mod_git_base/jenkinsfile/qa-k8s-env-for-production-project.sh db_query_project_type_all".execute().in.text;
 def alist=[];
 list.split(",").each{
     alist.add(it)
@@ -7,7 +7,7 @@ list.split(",").each{
 return alist;
 
 //project_name
-def list=" bash  /var/jenkins_home/jobs/qa-k8s-env-for-production-project/mod_git_base/jenkinsfile/qa-k8s-env-for-production-project.sh db_query_stripped_project_name_in_project_type ${project_type}".execute().in.text;
+def list=" bash /var/jenkins_home/jobs/qa-k8s-env-for-production-project/mod_git_base/jenkinsfile/qa-k8s-env-for-production-project.sh db_query_stripped_project_name_in_project_type ${project_type}".execute().in.text;
 def alist=[];
 list.split(",").each{
     alist.add(it)
@@ -15,7 +15,7 @@ list.split(",").each{
 return alist;
 
 //service_type
-def list=" bash  /var/jenkins_home/jobs/qa-k8s-env-for-production-project/mod_git_base/jenkinsfile/qa-k8s-env-for-production-project.sh db_query_service_type_in_project_type ${project_type}".execute().in.text;
+def list=" bash /var/jenkins_home/jobs/qa-k8s-env-for-production-project/mod_git_base/jenkinsfile/qa-k8s-env-for-production-project.sh db_query_service_type_in_project_type ${project_type}".execute().in.text;
 def alist=[];
 list.split(",").each{
     alist.add(it)
@@ -23,14 +23,14 @@ list.split(",").each{
 return alist;
 
 //git_path
-def list=" bash  /var/jenkins_home/jobs/qa-k8s-env-for-production-project/mod_git_base/jenkinsfile/qa-k8s-env-for-production-project.sh db_query_property ${project_type}-${project_name}-${service_type}".execute().in.text;
+def list=" bash /var/jenkins_home/jobs/qa-k8s-env-for-production-project/mod_git_base/jenkinsfile/qa-k8s-env-for-production-project.sh db_query_property git_path ${project_type}-${project_name}-${service_type}".execute().in.text;
 def alist=[];
 list.split(",").each{
     alist.add(it)
 }
 return alist;
 //git_branch
-def list=" bash  /var/jenkins_home/jobs/qa-k8s-env-for-production-project/mod_git_base/jenkinsfile/qa-k8s-env-for-production-project.sh git_branch ${project_type}-${project_name}-${service_type}".execute().in.text;
+def list=" bash /var/jenkins_home/jobs/qa-k8s-env-for-production-project/mod_git_base/jenkinsfile/qa-k8s-env-for-production-project.sh git_branch ${project_type}-${project_name}-${service_type}".execute().in.text;
 def alist=[];
 list.split(",").each{
     alist.add(it)
