6e347ecabf2b3ca084bf83b389f261b726d1a537
MYJCONAN-work-DELL <273987074@qq.com>
2023-12-12 09:40:20 +0800

    no message

--- jenkinsfile/qa-k8s-env-for-production-project.sh
+++ jenkinsfile/qa-k8s-env-for-production-project.sh
@@ -28,7 +28,7 @@ function db_query_project_type_all() {
     echo -e "查询project_type_all" >>$mod_git_base/jenkinsfile/log.log
     local col_name="project_name"
     local col_num=$(echo $(head -n 1 $project_database | awk -F "," -v col_name=$col_name '{ for (i=1; i<=NF; i++) if ($i == col_name) print i }'))
-    echo $(awk -v FS="," 'NR>1{print $'"$col_num"'}' $project_database | cut -d '-' -f1 | sort -u | xargs | sed 's/[ ][ ]*/,/g')
+    echo $(awk -v FS="," 'NR>1{print $'"$col_num"'}' $project_database | cut -d '-' -f1 | sort -u | xargs | sed 's/[ ][ ]*/,/g' | sed -i 's/$/,/')
 }
 
 function db_query_stripped_project_name_in_project_type() {
