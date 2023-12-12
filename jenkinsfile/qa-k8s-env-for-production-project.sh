aa6770a2320189dc8481de614cdcfd224e93225b
MYJCONAN-work-DELL <273987074@qq.com>
2023-12-12 15:35:35 +0800

    no message

--- jenkinsfile/qa-k8s-env-for-production-project.sh
+++ jenkinsfile/qa-k8s-env-for-production-project.sh
@@ -108,8 +108,8 @@ function init_build() {
     #清除上次构建镜像
     printf_std "清除上次构建镜像"
     rm -rf $mod_docker_image_path
-    cp -r $mod_git_base/mod_docker_image/qa-k8s-env-for-production-project-mod-server $mod_docker_image_prefix_path
-    mkdir -p $mod_docker_image_path/config
+    cp -r $mod_docker_image_path $mod_docker_image_prefix_path
+    mkdir -p $mod_docker_image_path/config/
 }
 
 #准备镜像
