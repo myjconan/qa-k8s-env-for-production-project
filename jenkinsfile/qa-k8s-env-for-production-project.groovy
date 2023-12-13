46d82d7dcbc723002bf7cc19e2dd80fbf8a8c14a
MYJCONAN-work-DELL <273987074@qq.com>
2023-12-13 11:13:35 +0800

    no message

--- jenkinsfile/qa-k8s-env-for-production-project.groovy
+++ jenkinsfile/qa-k8s-env-for-production-project.groovy
@@ -88,7 +88,7 @@ pipeline {
         stage('构建镜像'){
             steps{
 				script{
-                    sh "echo $(ls ${mod_docker_image_path}/config)"
+                    sh(script: "ls ${mod_docker_image_path}/config", returnStdout: true).trim()
 					sh "docker build -t '${harbor_url}/public/${app_name}:v1' ${mod_docker_image_path}/"
 				}
             }
