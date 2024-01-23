5be6e61507e3b5107cf7de00c44f9036c900d7e1
MYJCONAN-work-DELL <273987074@qq.com>
2024-01-23 18:01:05 +0800

    优化获取git分支代码

--- jenkinsfile/qa-k8s-env-for-production-project.sh
+++ jenkinsfile/qa-k8s-env-for-production-project.sh
@@ -94,7 +94,7 @@ function git_branch() {
     local project_id=$(db_query_property "git_id" $project_name)
     # printf_std "查询${project_name}的git分支，git_id为${project_id}"
     for i in $(seq 1 11); do
-        local branch_list=$(curl -s --header "PRIVATE-TOKEN: ${token}" "http://gitlab.dahantc.com/api/v4/projects/${project_id}/repository/branches?per_page=200&page=$i" | jq -r ".[].name" | grep -Ev "master" | tr "\n" "," | sed 's/,$//g')
+        local branch_list=$(curl -s --header "PRIVATE-TOKEN: ${token}" "http://gitlab.dahantc.com/api/v4/projects/${project_id}/repository/branches?per_page=200&page=$i" | jq -r ".[].name" | tr "\n" "," | sed 's/,$//g')
         echo "master",$branch_list
     done
 }
