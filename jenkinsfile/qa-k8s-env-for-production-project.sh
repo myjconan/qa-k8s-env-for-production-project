6199521f2ccbd75b24dca065ab16702a83f9a95d
MYJCONAN-work-DELL <273987074@qq.com>
2024-05-06 17:51:29 +0800

    修改gitlab的token获取方式

--- jenkinsfile/qa-k8s-env-for-production-project.sh
+++ jenkinsfile/qa-k8s-env-for-production-project.sh
@@ -96,7 +96,6 @@ function git_branch() {
     local project_name=${1:-1}
     echo -e "$(date '+%Y-%m-%d %H:%M:%S') 查询git_branch：$project_name" >>$mod_git_base/jenkinsfile/log.log
     local token=$(echo $(cat /var/jenkins_home/workspace/jenkins/get_project_branch.sh.bak | grep -E 'token=' | awk -F "=" 'NR==1{print $2}' | sed 's/"//g'))
-    echo $token
     local gitlab_url="gitlab.dahantc.com"
     local project_id=$(db_query_property "git_id" $project_name)
     local total_git_branch_list="master"
