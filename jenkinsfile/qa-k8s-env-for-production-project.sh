3916be5010d936e3886fadceabb451f7f3ec0954
MYJCONAN-work-DELL <273987074@qq.com>
2024-08-19 10:46:58 +0800

    新增6个项目

--- jenkinsfile/qa-k8s-env-for-production-project.sh
+++ jenkinsfile/qa-k8s-env-for-production-project.sh
@@ -14,7 +14,7 @@ mysql_path="/var/jenkins_home/jobs/qa-k8s-env-for-production-project/mysql/"
 # 中间件连接信息
 declare -A property
 #database
-property['db_host']="172.18.1.79"
+property['db_host']="172.18.1.100"
 property['db_port']="3306"
 property['db_root_password']="123456"
 #redis
